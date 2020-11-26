5e5fbf5fa6752015309a7e90d5f790925ec30ee1
Mew <1258503426@qq.com>
2020-11-26 17:28:14 +0800

    第二天来的新人b上传

---
+++ index.vue
@@ -0,0 +1,22 @@
+<template>
+<h1>111111111111111</h1>
+</template>
+<script>
+ import {mapGetters,mapActions} from 'vuex';
+export default {
+props: [],
+components: {},
+data() {
+return {};
+},
+computed: {
+...mapGetters({}),
+},
+methods: {
+...mapActions({}),
+},
+mounted() {},
+};
+</script>
+<style scoped>
+</style>
